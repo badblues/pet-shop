ede91b0d5027f7fa79a17e3d9f67fadaa8bd918a
badbadblues _ <borovikxv.a@gmail.com>
2023-10-19 17:31:02 +0700

    init commit

---
+++ Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+
+string connectionString = "User ID=postgres;Password=root;Host=localhost;Port=5432;Database=rpbd;";
+
+var config = new Configuration();
+
+config.DataBaseIntegration(d =>
+{
+    d.ConnectionString = connectionString;
+    d.Dialect<PostgreSQLDialect>();
+    d.Driver<NpgsqlDriver>();
+});
+
+config.AddAssembly(Assembly.GetExecutingAssembly());
+var sessionFactory = config.BuildSessionFactory();
+
+using (var session = sessionFactory.OpenSession())
+{
+    Console.WriteLine("Session opened!");
+
+}
---
+++ Persistence/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Persistence.Models;
+
+[Table("Clients")]
+internal class Client
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    [Required]
+    public string Address { get; set; } = string.Empty;
+
+}
