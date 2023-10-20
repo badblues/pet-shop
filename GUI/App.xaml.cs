3b2c89bd333b1faeb136e0d9ae9d16fb270127c2
badbadblues _ <borovikxv.a@gmail.com>
2023-10-20 09:43:56 +0700

    remove console app, configure wpf with DI

--- GUI/App.xaml.cs
+++ GUI/App.xaml.cs
@@ -1,15 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Cfg;
+using GUI.View;
+using GUI.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Persistence.Repositories;
+using Persistence.Mappings;
+using NHibernate;
 
 namespace GUI;
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
+
 public partial class App : Application
 {
+    private readonly IHost _host;
+    private readonly ISession _session;
+
+    private string _connectionString = "User ID=postgres;Password=root;Host=localhost;Port=5432;Database=rpbd;";
+
+    public App()
+    {
+        var config = Fluently.Configure()
+            .Database(PostgreSQLConfiguration.PostgreSQL82
+                .ConnectionString(_connectionString)
+                .ShowSql()) // Show SQL in the console (for debugging)
+            .Mappings(m =>
+            {
+                m.FluentMappings
+                    .Add<ClientMap>();
+            })
+            .BuildConfiguration();
+
+        var sessionFactory = config.BuildSessionFactory();
+        _session = sessionFactory.OpenSession();
+
+        _host = Host.CreateDefaultBuilder()
+        .ConfigureServices((services) =>
+        {
+            services.AddSingleton<MainWindow>();
+            services.AddScoped(provider => new ClientRepository(_session));
+            services.AddSingleton<MainVM>();
+        })
+        .Build();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _host.Start();
+
+        MainWindow = _host.Services.GetRequiredService<MainWindow>();
+        MainWindow.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _host.StopAsync();
+        _host.Dispose();
+
+        base.OnExit(e);
+    }
 }
---
+++ GUI/View/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using GUI.ViewModels;
+
+namespace GUI.View;
+
+public partial class MainWindow : Window
+{
+    public MainWindow(MainVM mainVM)
+    {
+        DataContext = mainVM;
+        InitializeComponent();
+    }
+}
