d56f1b6fad4444c0660443e1a6559a8d67031bbf
badbadblues _ <borovikxv.a@gmail.com>
2023-10-30 17:03:17 +0700

    add animal model, mapping, repository

---
+++ Persistence/Models/Animal.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Persistence.Models;
+
+[Table("Animals")]
+public class Animal
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public virtual int Id { get; set; }
+
+    [Required]
+    public virtual required string Name { get; set; }
+
+    public virtual int Age { get; set; }
+
+    [Required]
+    public virtual Gender Gender { get; set; }
+
+    [Required]
+    [ForeignKey("breed_id")]
+    public virtual int BreedId { get; set; }
+
+    public virtual required Breed Breed { get; set; }
+
+    public virtual required string ExteriorDescription { get; set; }
+
+    public virtual required string Pedigree { get; set; }
+
+    public virtual required string Veterinarian { get; set; }
+
+    [ForeignKey("owner_id")]
+    public virtual int ClientId { get; set; }
+
+    public virtual required Client Client { get; set; }
+}
---
+++ Persistence/Repositories/AnimalRepository.cs
@@ -0,0 +1,42 @@
+﻿using NHibernate;
+using Persistence.Models;
+
+namespace Persistence.Repositories;
+
+public class AnimalRepository : IRepository<Animal>
+{
+    private readonly ISession _session;
+
+    public AnimalRepository(ISession session)
+    {
+        _session = session;
+    }
+
+    public void Add(Animal animal)
+    {
+        _ = _session.Save(animal);
+    }
+
+    public Animal Get(int id)
+    {
+        _session.Flush();
+        return _session.Get<Animal>(id);
+    }
+
+    public IEnumerable<Animal> GetAll()
+    {
+        _session.Flush();
+        return _session.Query<Animal>().ToList();
+    }
+
+    public void Update(Animal animal)
+    {
+        _session.Update(animal);
+    }
+
+    public void Delete(int id)
+    {
+        Animal animal = _session.Get<Animal>(id);
+        _session.Delete(animal);
+    }
+}
