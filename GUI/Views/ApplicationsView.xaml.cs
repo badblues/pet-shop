5391183550c606a1b569a325318375b608ecedf1
badbadblues _ <borovikxv.a@gmail.com>
2023-10-30 18:40:38 +0700

    code cleanup

--- GUI/Views/ApplicationsView.xaml.cs
+++ GUI/Views/ApplicationsView.xaml.cs
@@ -2,9 +2,6 @@
 
 namespace GUI.Views;
 
-/// <summary>
-/// Interaction logic for ApplicationsView.xaml
-/// </summary>
 public partial class ApplicationsView : UserControl
 {
     public ApplicationsView()
