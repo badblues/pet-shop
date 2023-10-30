cc74ce410c119426b8ee916a6f1e5a02881f6ff9
badbadblues _ <borovikxv.a@gmail.com>
2023-10-30 17:39:34 +0700

    fix context

--- GUI/Views/ClientsView.xaml.cs
+++ GUI/Views/ClientsView.xaml.cs
@@ -10,47 +10,53 @@ namespace GUI.Views;
 
 public partial class ClientsView : UserControl
 {
-    private readonly ClientsViewModel _context;
 
     public ClientsView()
     {
+        ClientsViewModel context = (ClientsViewModel)DataContext;
         InitializeComponent();
-        _context = (ClientsViewModel)DataContext;
+        context = (ClientsViewModel)DataContext;
     }
 
     private void Clients_MouseLeftButton(object sender, MouseButtonEventArgs e)
     {
+        ClientsViewModel context = (ClientsViewModel)DataContext;
         DockPanel dockPanel = (DockPanel)sender;
         Client? client = dockPanel.DataContext as Client;
-        _context.SelectClient(client);
+        context.SelectClient(client);
     }
 
     private void AddClient_Click(object sender, RoutedEventArgs e)
     {
-        _context.AddClient();
+        ClientsViewModel context = (ClientsViewModel)DataContext;
+        context.AddClient();
     }
 
     private void RichTextBoxName_TextChanged(object sender, TextChangedEventArgs e)
     {
+        ClientsViewModel context = (ClientsViewModel)DataContext;
         string text = new TextRange(richTextBoxName.Document.ContentStart, richTextBoxName.Document.ContentEnd).Text;
         text = text.TrimEnd('\r', '\n');
-        _context.ChangeNameText(text);
+        context.ChangeNameText(text);
     }
 
     private void RichTextBoxAddress_TextChanged(object sender, TextChangedEventArgs e)
     {
+        ClientsViewModel context = (ClientsViewModel)DataContext;
         string text = new TextRange(richTextBoxAddress.Document.ContentStart, richTextBoxAddress.Document.ContentEnd).Text;
         text = text.TrimEnd('\r', '\n');
-        _context.ChangeAddressText(text);
+        context.ChangeAddressText(text);
     }
 
     private void ClientProfile_DeleteClicked(object sender, ResourceEventArgs<Client> e)
     {
-        _context.DeleteClient(e.Resource);
+        ClientsViewModel context = (ClientsViewModel)DataContext;
+        context.DeleteClient(e.Resource);
     }
 
     private void ClientProfile_UpdateClicked(object sender, ResourceEventArgs<Client> e)
     {
-        _context.UpdateClient(e.Resource);
+        ClientsViewModel context = (ClientsViewModel)DataContext;
+        context.UpdateClient(e.Resource);
     }
 }
