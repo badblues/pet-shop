b1ebe54c91d47be6b24e0b191764a3379b13d754
badbadblues _ <borovikxv.a@gmail.com>
2023-10-30 18:03:09 +0700

    add participation model, mapping, repository

---
+++ Persistence/Mappings/ParticipationMapping.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Mapping;
+using Persistence.Models;
+
+namespace Persistence.Mappings;
+
+public class ParticipationMapping : ClassMap<Participation>
+{
+    public ParticipationMapping()
+    {
+        Table("Participations");
+
+        CompositeId()
+           .KeyReference(x => x.Animal, "animal_id")
+           .KeyReference(x => x.Competition, "competition_id");
+
+        Map(x => x.Award, "award").Not.Nullable();
+    }
+}
---
+++ Persistence/Models/Participation.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Persistence.Models;
+
+public class Participation
+{
+    [Required]
+    public virtual int AnimalId { get; set; }
+
+    [Required]
+    public virtual required Animal Animal { get; set; }
+
+    [Required]
+    public virtual int CompetitionId { get; set; }
+
+    [Required]
+    public virtual required Competition Competition { get; set; }
+
+    [Required]
+    public virtual string Award {  get; set; }
+
+}
